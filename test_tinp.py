eba7dea3fde43c0fdc1d5ef89b6f6d59bb6dbd55
dokelung <dokelung@gmail.com>
2017-05-07 23:39:40 +0800

    add new test

--- test_tinp.py
+++ test_tinp.py
@@ -11,9 +11,13 @@ class TestTinFunctions(unittest.TestCase):
 
     @patch('builtins.input', return_value='John,  30, 70.5')
     def test_finput(self, input):
-        self.assertEqual(finput(fstr='%s, *%d, *%f'), ('John', 30, 70.5))
-        self.assertRaises(InputDoesNotMatchFStr, finput, fstr='%s, %d, %f')
-        self.assertRaises(TypeConvertError, finput, fstr='%d, *%d, *%f')
+        self.assertEqual(finput(fstr='%s, *%d, *%f', re_escape=False), ('John', 30, 70.5))
+        self.assertRaises(InputDoesNotMatchFStr, finput, fstr='%s, %d, %f', re_escape=False)
+        self.assertRaises(TypeConvertError, finput, fstr='%d, *%d, *%f', re_escape=False)
+
+    @patch('builtins.input', return_value='5+5')
+    def test_finput2(self, input):
+        self.assertEqual(finput(fstr='%d+%d'), (5, 5))
 
     @patch('builtins.input', return_value='1 2 3 4 5')
     def test_tinput(self, input):
